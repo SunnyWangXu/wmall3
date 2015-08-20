d73b4228aa10f02a2eca6831c354e77868df0749
兰超 <lanchao0713@msn.com>
2015-08-20 10:39:38 +0800

    增加MainApplication

---
+++ app/src/main/java/com/wjhgw/MainApplication.java
@@ -0,0 +1,14 @@
+package com.wjhgw;
+
+import android.app.Application;
+
+/**
+ * Created by Lance on 15/8/19.
+ */
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
