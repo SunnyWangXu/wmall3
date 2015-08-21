4b2a1833626d3715c39a7d3fdf2ddbc401676b33
denghx <472487679@qq.com>
2015-08-21 16:52:09 +0800

    封装Volley异步加载图片方法，增加自定义圆形Imageview

--- app/src/main/java/com/wjhgw/ui/fragment/IndexFragment.java
+++ app/src/main/java/com/wjhgw/ui/fragment/IndexFragment.java
@@ -6,13 +6,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.android.volley.toolbox.NetworkImageView;
 import com.wjhgw.R;
+import com.wjhgw.ui.image.LoadImageByVolley;
+
 public class IndexFragment extends Fragment {
 	View messageLayout;
+	private ImageView mImageView;
+	private NetworkImageView mNetworkImageView;
+	private LoadImageByVolley load;
 	Intent intent;
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		messageLayout = inflater.inflate(R.layout.index_layout, container, false);
+		mImageView = (ImageView)messageLayout.findViewById(R.id.imageView);
+		mNetworkImageView = (NetworkImageView) messageLayout.findViewById(R.id.networkImageView);
+		load = new LoadImageByVolley(getActivity(),mImageView);
+		load.loadImageByVolley("http://avatar.csdn.net/6/6/D/1_lfdfhl.jpg");
 		return messageLayout;
 	}
 
---
+++ app/src/main/java/com/wjhgw/ui/image/LoadImageByVolley.java
@@ -0,0 +1,50 @@
+package com.wjhgw.ui.image;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
+import android.widget.ImageView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+import com.wjhgw.R;
+
+/**
+ * 利用Volley异步加载图片
+ *
+ * 注意方法参数:
+ * getImageListener(ImageView view, int defaultImageResId, int errorImageResId)
+ * 第一个参数:显示图片的ImageView
+ * 第二个参数:默认显示的图片资源
+ * 第三个参数:加载错误时显示的图片资源
+ */
+public class LoadImageByVolley {
+
+    private Context mContext;
+    private ImageView mImageView;
+
+    public LoadImageByVolley(Context context,ImageView view) {
+        mContext = context;
+        mImageView = view;
+    }
+
+    public void loadImageByVolley(String imageUrl){
+        RequestQueue requestQueue = Volley.newRequestQueue(mContext);
+        final LruCache<String, Bitmap> lruCache = new LruCache<String, Bitmap>(20);
+        ImageLoader.ImageCache imageCache = new ImageLoader.ImageCache() {
+            @Override
+            public void putBitmap(String key, Bitmap value) {
+                lruCache.put(key, value);
+            }
+
+            @Override
+            public Bitmap getBitmap(String key) {
+                return lruCache.get(key);
+            }
+        };
+        ImageLoader imageLoader = new ImageLoader(requestQueue, imageCache);
+        ImageLoader.ImageListener listener = ImageLoader.getImageListener(mImageView, R.mipmap.ic_launcher,R.mipmap.ic_launcher);
+        imageLoader.get(imageUrl, listener);
+    }
+}
