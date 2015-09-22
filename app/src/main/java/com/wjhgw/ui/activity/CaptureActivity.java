253db05d697df2406457d45bcdcba83433d1c9bf
denghx <472487679@qq.com>
2015-09-22 15:34:07 +0800

    集成Zxing扫码功能，初步分类的标题的UI

---
+++ app/src/main/java/com/wjhgw/ui/activity/CaptureActivity.java
@@ -0,0 +1,170 @@
+package com.wjhgw.ui.activity;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Result;
+import com.wjhgw.R;
+import com.wjhgw.ui.fragment.ClassificationFragment;
+import com.wjhgw.utils.Zxing.camera.CameraManager;
+import com.wjhgw.utils.Zxing.decoding.CaptureActivityHandler;
+import com.wjhgw.utils.Zxing.decoding.InactivityTimer;
+import com.wjhgw.utils.Zxing.view.ViewfinderView;
+
+import java.io.IOException;
+import java.util.Vector;
+
+public class CaptureActivity extends Activity implements Callback {
+
+    private CaptureActivityHandler handler;
+    private ViewfinderView viewfinderView;
+    private boolean hasSurface;
+    private Vector<BarcodeFormat> decodeFormats;
+    private String characterSet;
+    private TextView txtResult;
+    private InactivityTimer inactivityTimer;
+    private MediaPlayer mediaPlayer;
+    private boolean playBeep;
+    private static final float BEEP_VOLUME = 0.10f;
+    private boolean vibrate;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.capture);
+        CameraManager.init(getApplication());
+
+        viewfinderView = (ViewfinderView) findViewById(R.id.viewfinder_view);
+        txtResult = (TextView) findViewById(R.id.txtResult);
+        hasSurface = false;
+        inactivityTimer = new InactivityTimer(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.preview_view);
+        SurfaceHolder surfaceHolder = surfaceView.getHolder();
+        if (hasSurface) {
+            initCamera(surfaceHolder);
+        } else {
+            surfaceHolder.addCallback(this);
+            surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
+        decodeFormats = null;
+        characterSet = null;
+
+        playBeep = true;
+        AudioManager audioService = (AudioManager) getSystemService(AUDIO_SERVICE);
+        if (audioService.getRingerMode() != AudioManager.RINGER_MODE_NORMAL) {
+            playBeep = false;
+        }
+        vibrate = true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (handler != null) {
+            handler.quitSynchronously();
+            handler = null;
+        }
+        CameraManager.get().closeDriver();
+    }
+
+    @Override
+    protected void onDestroy() {
+        inactivityTimer.shutdown();
+        super.onDestroy();
+    }
+
+    private void initCamera(SurfaceHolder surfaceHolder) {
+        try {
+            CameraManager.get().openDriver(surfaceHolder);
+        } catch (IOException ioe) {
+            return;
+        } catch (RuntimeException e) {
+            return;
+        }
+        if (handler == null) {
+            handler = new CaptureActivityHandler(this, decodeFormats,
+                    characterSet);
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if (!hasSurface) {
+            hasSurface = true;
+            initCamera(holder);
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        hasSurface = false;
+    }
+
+    public ViewfinderView getViewfinderView() {
+        return viewfinderView;
+    }
+
+    public Handler getHandler() {
+        return handler;
+    }
+
+    public void drawViewfinder() {
+        viewfinderView.drawViewfinder();
+
+    }
+
+    public void handleDecode(Result obj, Bitmap barcode) {
+        inactivityTimer.onActivity();
+        viewfinderView.drawResultBitmap(barcode);
+        /**
+         * 播放成功的音频
+         */
+        mediaPlayer = MediaPlayer.create(this, R.raw.beep);
+        mediaPlayer.start();
+        /*txtResult.setText(obj.getBarcodeFormat().toString() + ":"
+                + obj.getText());*/
+
+        ClassificationFragment.RESULT_MESSAGE = obj.getText();
+        if (ClassificationFragment.RESULT_MESSAGE != null) {
+            finish();
+        }
+
+        //Intent intent = new Intent(CaptureActivity.this, IndexActivity.class);
+        //this.startActivity(intent);
+        //this.finish();
+    }
+
+    /**
+     * When the beep has finished playing, rewind to queue up another one.
+     */
+    private final OnCompletionListener beepListener = new OnCompletionListener() {
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            mediaPlayer.seekTo(0);
+        }
+    };
+
+}
--- app/src/main/java/com/wjhgw/ui/fragment/ClassificationFragment.java
+++ app/src/main/java/com/wjhgw/ui/fragment/ClassificationFragment.java
@@ -1,5 +1,6 @@
 package com.wjhgw.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -15,6 +16,7 @@ import com.wjhgw.base.BaseQuery;
 import com.wjhgw.business.api.Classification_Request;
 import com.wjhgw.business.response.BusinessResponse;
 import com.wjhgw.config.ApiInterface;
+import com.wjhgw.ui.activity.CaptureActivity;
 import com.wjhgw.ui.image.LoadImageByVolley;
 import com.wjhgw.ui.view.listview.MyListView;
 import com.wjhgw.ui.view.listview.XListView;
@@ -26,15 +28,17 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 
-public class ClassificationFragment extends Fragment implements XListView.IXListViewListener, BusinessResponse {
+public class ClassificationFragment extends Fragment implements XListView.IXListViewListener, BusinessResponse ,View.OnClickListener {
     public static int MAK = 0;
     private ImageView image;
+    private ImageView qrcode_scanner;
     private MyListView mListView;
     private goods_class_adapter adapter;
     private grid_adapter adapter1;
     private Classification_Request dataModel;
     private GridView grid;
     private LoadImageByVolley Image;
+    public static String RESULT_MESSAGE = null; // 接收扫二维码返回数据
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -47,9 +51,12 @@ public class ClassificationFragment extends Fragment implements XListView.IXList
         hashMap.put("key", "ed86e0e4dd9fd54ffea7511b9fc6728e");
         dataModel.goods_class(hashMap, BaseQuery.serviceUrl() + ApiInterface.Goods_class);
 
-        mListView = (MyListView) View.findViewById(R.id.discovery_listview);
+
         grid = (GridView) View.findViewById(R.id.grid);
         image = (ImageView) View.findViewById(R.id.image);
+        qrcode_scanner = (ImageView) View.findViewById(R.id.qrcode_scanner);
+
+        mListView = (MyListView) View.findViewById(R.id.discovery_listview);
         mListView.setPullLoadEnable(false);
         mListView.setPullRefreshEnable(false);
         mListView.setXListViewListener(this, 1);
@@ -68,6 +75,8 @@ public class ClassificationFragment extends Fragment implements XListView.IXList
                 dataModel.goods_class1(hashMap, BaseQuery.serviceUrl() + ApiInterface.Goods_class1);
             }
         });
+
+        qrcode_scanner.setOnClickListener(this);
         return View;
     }
 
@@ -103,4 +112,46 @@ public class ClassificationFragment extends Fragment implements XListView.IXList
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.qrcode_scanner:
+                Intent intent = new Intent(getActivity(), CaptureActivity.class);
+                getActivity().startActivity(intent);
+                break;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (RESULT_MESSAGE != null) {
+            /*try {
+                URL myURL = new URL(RESULT_MESSAGE);
+                if (myURL.getProtocol().equals("http")
+                        || myURL.getProtocol().equals("https")
+                        || myURL.getProtocol().equals("ftp")) {
+                    if (myURL.getPath().equals("/mobile/goods.php")
+                            || myURL.getPath().equals("/goods.php")) {
+                        Intent it = new Intent(getActivity(),
+                                B2_ProductDetailActivity.class);
+                        it.putExtra(
+                                "good_id",
+                                myURL.getQuery().substring(3,
+                                        myURL.getQuery().length()));
+                        this.startActivity(it);
+                    } else {
+                        Intent intent = new Intent(getActivity(),
+                                BannerWebActivity.class);
+                        intent.putExtra("url", RESULT_MESSAGE);
+                        startActivity(intent);
+                    }
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }*/
+            Toast.makeText(getActivity(), "扫描返回"+RESULT_MESSAGE, Toast.LENGTH_SHORT).show();
+            RESULT_MESSAGE = null;
+        }
+    }
 }
