1baeba8d7417a6536475711c5e93536db30479bb
denghx <472487679@qq.com>
2015-09-23 18:22:32 +0800

    初步实现主题街的首页UI

--- app/src/main/java/com/wjhgw/ui/fragment/IndexFragment.java
+++ app/src/main/java/com/wjhgw/ui/fragment/IndexFragment.java
@@ -30,6 +30,7 @@ public class IndexFragment extends Fragment implements BusinessResponse, IXListV
     private View messageLayout;
     private LinearLayout MenuLayout;
     private LinearLayout Eventlayout;
+    private LinearLayout Themelayout;
     private ImageView mImageView;
     private LoadImageByVolley load;
     private Goods_Request dataModel;
@@ -89,6 +90,7 @@ public class IndexFragment extends Fragment implements BusinessResponse, IXListV
         mListView.addHeaderView(indexViewPageLayout);
         mListView.addHeaderView(MenuLayout);
         mListView.addHeaderView(Eventlayout);
+        mListView.addHeaderView(Themelayout);
     }
 
     /**
@@ -98,6 +100,7 @@ public class IndexFragment extends Fragment implements BusinessResponse, IXListV
     private void setInflaterView() {
         MenuLayout = (LinearLayout) LayoutInflater.from(getActivity()).inflate(R.layout.index_menu, null);
         Eventlayout = (LinearLayout) LayoutInflater.from(getActivity()).inflate(R.layout.event_layout, null);
+        Themelayout = (LinearLayout) LayoutInflater.from(getActivity()).inflate(R.layout.theme_layout, null);
         indexViewPageLayout = (RelativeLayout) LayoutInflater.from(getActivity()).inflate(R.layout.index_page_layout, null);
         indexPager = (ViewPager) indexViewPageLayout.findViewById(R.id.pager);
         mPagerAdapter = new IndexPagerAdapter(getActivity());
