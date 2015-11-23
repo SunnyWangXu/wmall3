9090ad7627316412e77b20808cc4fb2c0e809bfb
wangxu <898383685@qq.com>
2015-11-23 11:41:43 +0800

    1.解决分类界面的商品分类属性数据拿到有混乱重复的BUG

--- app/src/main/java/com/wjhgw/ui/view/listview/adapter/AttrAdapter.java
+++ app/src/main/java/com/wjhgw/ui/view/listview/adapter/AttrAdapter.java
@@ -22,12 +22,8 @@ import java.util.List;
 public class AttrAdapter extends BaseAdapter {
     private Context mContext;
     private ArrayList<Goods_attr_data> data;
-    private ViewHolder vh = new ViewHolder();
     private TextView tvAttrCiecle;
 
-    private static class ViewHolder {
-        private TextView text;
-    }
 
     public AttrAdapter(Context c, ArrayList<Goods_attr_data> data) {
         this.mContext = c;
@@ -53,50 +49,49 @@ public class AttrAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        TextView goodsName;
-        GridView goodsValueGV;
+
+        ViewHolder holder = null;
         if (convertView == null) {
+            holder = new ViewHolder();
 
             convertView = LayoutInflater.from(mContext).inflate(R.layout.goods_attr_value, null);
-            goodsName = (TextView) convertView.findViewById(R.id.tv_goods_attr_name);
-            goodsName.setText(data.get(position).name);
-
-            goodsValueGV = (GridView) convertView.findViewById(R.id.gv_goods_attr_value);
+            holder.goodsNmae = (TextView) convertView.findViewById(R.id.tv_goods_attr_name);
+            holder.goodsValueGV = (GridView) convertView.findViewById(R.id.gv_goods_attr_value);
 
             tvAttrCiecle = (TextView) convertView.findViewById(R.id.tv_attr_circle);
 
-         if((position % 5) == 1){
-             tvAttrCiecle.setBackgroundResource(R.drawable.attr_rose_shape);
-            }else if((position % 5) == 2){
-             tvAttrCiecle.setBackgroundResource(R.drawable.attr_purple_shape);
-            }else if((position % 5) == 3){
-             tvAttrCiecle.setBackgroundResource(R.drawable.attr_blue_shape);
-            }else if((position % 5) == 4){
-             tvAttrCiecle.setBackgroundResource(R.drawable.attr_yellow_shape);
-            }
-
-            //如果是第一条 热门品牌查询的字段不一样 专门设置适配器，其他的设置另外的适配器
-            if (position == 0) {
-                List<Goods_attr_data_value> dataValues = data.get(position).value;
-                AttrValueGVAdapter1 attrValueGVAdapter1 = new AttrValueGVAdapter1(mContext, dataValues);
-                goodsValueGV.setAdapter(attrValueGVAdapter1);
-            } else {
-                List<Goods_attr_data_value> dataValues = data.get(position).value;
-                AttrValueGVAdapter2 attrValueGVAdapter2 = new AttrValueGVAdapter2(mContext, dataValues);
-                goodsValueGV.setAdapter(attrValueGVAdapter2);
-            }
-
+            convertView.setTag(holder);
         }
+
+        holder = (ViewHolder) convertView.getTag();
+        holder.goodsNmae.setText(data.get(position).name);
+
+        if ((position % 5) == 1) {
+            tvAttrCiecle.setBackgroundResource(R.drawable.attr_rose_shape);
+        } else if ((position % 5) == 2) {
+            tvAttrCiecle.setBackgroundResource(R.drawable.attr_purple_shape);
+        } else if ((position % 5) == 3) {
+            tvAttrCiecle.setBackgroundResource(R.drawable.attr_blue_shape);
+        } else if ((position % 5) == 4) {
+            tvAttrCiecle.setBackgroundResource(R.drawable.attr_yellow_shape);
+        }
+
+        //如果是第一条 热门品牌查询的字段不一样 专门设置适配器，其他的设置另外的适配器
+        if (position == 0) {
+            List<Goods_attr_data_value> dataValues = data.get(position).value;
+            AttrValueGVAdapter1 attrValueGVAdapter1 = new AttrValueGVAdapter1(mContext, dataValues);
+            holder.goodsValueGV.setAdapter(attrValueGVAdapter1);
+        } else {
+            List<Goods_attr_data_value> dataValues = data.get(position).value;
+            AttrValueGVAdapter2 attrValueGVAdapter2 = new AttrValueGVAdapter2(mContext, dataValues);
+            holder.goodsValueGV.setAdapter(attrValueGVAdapter2);
+        }
+
         return convertView;
     }
 
+    public class ViewHolder {
+        TextView goodsNmae;
+        GridView goodsValueGV;
+    }
 }
-//    @Override
-//    public View getView(int position, View attrValue, ViewGroup parent) {
-//        attrValue = LayoutInflater.from(mContext).inflate(R.layout.goods_attr_value, null);
-//        vh.text = (TextView) attrValue.findViewById(R.id.goods_class_item);
-//        vh.text.setText(List.get(position).gc_name);
-//        attrValue.setTag(vh);
-//        return attrValue;
-//    }
-//}
