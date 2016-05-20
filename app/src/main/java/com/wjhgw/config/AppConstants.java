21b69a67bdcd885e14304c0f07540a74d8871ac0
denghx <472487679@qq.com>
2016-05-20 10:17:14 +0800

    修改连接线上服务器链接

--- app/src/main/java/com/wjhgw/config/AppConstants.java
+++ app/src/main/java/com/wjhgw/config/AppConstants.java
@@ -2,6 +2,6 @@ package com.wjhgw.config;
 
 public class AppConstants
 {
-    public static final String SERVER_PRODUCTION = "http://dev.wjhgw.com";
+    public static final String SERVER_PRODUCTION = "http://www.wjhgw.com";
     public static final String SERVER_DEVELOPMENT = "http://10.10.0.143";
 }
